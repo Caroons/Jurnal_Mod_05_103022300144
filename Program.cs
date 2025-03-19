65befd7afbb7d225b26fa76b64c64624d1fa87b6
Calderaly <calderaly16@gmail.com>
2025-03-19 07:13:57 +0700

    feat(Pemroses Data): menambahkan class PemrosesData dengan method DapatkanNilaiTerbesar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class PemrosesData
+{
+    public T DapatkanNilaiTerbesar<T>(T? num1, T? num2, T num3)
+    {
+        dynamic? temp1 = num1;
+        dynamic? temp2 = num2;
+        dynamic? temp3 = num3;
+
+        dynamic? largest = temp1;
+
+        if (temp2 > largest)
+        {
+            largest = temp2;
+        }
+        
+        if (temp3 > largest)
+        {
+            largest = temp3;
+        }
+
+        return largest!;
+    }
+}
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        PemrosesData pemroses = new PemrosesData();
+        Console.WriteLine(pemroses.DapatkanNilaiTerbesar<double>(1, 2, 3));
+    }
+}
