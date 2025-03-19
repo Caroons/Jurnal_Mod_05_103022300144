6ddeec5fb17052c50444bd242ff42bc0960114e3
Calderaly <calderaly16@gmail.com>
2025-03-19 07:37:54 +0700

    feat(SimpleDataBase): menambahkan clas SimpledataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData<T>(T input)
+    {
+        dynamic? convert = input;
+
+        storedData.Add(convert);
+        inputDates.Add(DateTime.Now);    
+    }
+
+    public void PrintAllData()
+    {
+        for(int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data1 berisi: "+ storedData[i] + ", " + "yang disimpan pada waktu " + inputDates[i].ToString());
+        }
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args) {
+        SimpleDataBase<double?> simpleDB = new();
+        simpleDB.AddNewData<double?>(12);
+        simpleDB.AddNewData<double?>(34);
+        simpleDB.AddNewData<double?>(56);
+        simpleDB.PrintAllData();
+    }
+}
